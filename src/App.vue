311cb2f27c913cb9ccf72060a76c03e84e70ba61
Yuyun Yuliana <yuyunsimdigmm17@g,ail.com>
2023-08-22 14:28:17 +0700

    initial commited

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <Clock />
+  </div>
+</template>
+
+<script>
+import Clock from "./components/Clock.vue";
+
+export default {
+  name: "App",
+  components: {
+    Clock
+  }
+};
+</script>
---
+++ src/components/Clock.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="clock">
+    <h1>{{ currentTime }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      currentTime: new Date().toLocaleTimeString()
+    };
+  },
+  created() {
+    setInterval(() => {
+      this.currentTime = new Date().toLocaleTimeString();
+    }, 1000);
+  }
+};
+</script>
+
+<style>
+.clock {
+  text-align: center;
+  font-size: 2rem;
+  margin-top: 2rem;
+}
+</style>
